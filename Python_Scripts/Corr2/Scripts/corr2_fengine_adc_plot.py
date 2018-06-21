b5f034b10421147a30f61a6da65a2cdc9b5c7c9e
Andrewvdb <avanderbyl@ska.ac.za>
2018-06-21 19:08:59 +0200

    Modified test scripts. --vacc option added

---
+++ Python_Scripts/Corr2/Scripts/corr2_fengine_adc_plot.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable-msg=C0103
+# pylint: disable-msg=C0301
+"""
+Plot the ADC data on one or more F-engines
+"""
+import time
+import argparse
+import os
+import sys
+import signal
+import numpy
+
+# force the use of the TkAgg backend
+import matplotlib
+matplotlib.use('TkAgg')
+
+from matplotlib import pyplot
+
+import casperfpga.memory as caspermem
+from corr2 import utils
+from corr2 import fhost_fpga
+
+parser = argparse.ArgumentParser(
+    description='Plot a histogram of the incoming ADC data for a fengine host.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    dest='host', type=str, action='store', default='',
+    help='the F-engine host to query, if an int, will be taken positionally '
+         'from the config file list of f-hosts')
+parser.add_argument(
+    '--config', dest='config', type=str, action='store', default='',
+    help='a corr2 config file, will use $CORR2INI if none given')
+parser.add_argument(
+    '--bitstream', dest='bitstream', type=str, action='store', default='',
+    help='a bitstream for the f-engine hosts (Skarabs need this given '
+         'if no config is found)')
+parser.add_argument(
+    '--range', dest='range', action='store', default='-1,-1',
+    help='range to plot, -1 means start/end')
+parser.add_argument(
+    '--hist', dest='hist', action='store_true', default=False,
+    help='plot a histogram of the incoming data')
+parser.add_argument(
+    '--fft', dest='fft', action='store_true', default=False,
+    help='perform a soft FFT on the data before plotting')
+parser.add_argument(
+    '--integrate', dest='integrate', type=int, action='store', default=0,
+    help='fft option - how many spectra should be integrated')
+parser.add_argument(
+    '--linear', dest='linear', action='store_true', default=False,
+    help='plot linear, not log')
+parser.add_argument(
+    '--noplot', dest='noplot', action='store_true', default=False,
+    help='do not plot, dump values to screen')
+parser.add_argument(
+    '--loglevel', dest='log_level', action='store', default='INFO',
+    help='log level to use, options INFO, DEBUG, ERROR')
+args = parser.parse_args()
+
+if args.log_level != '':
+    import logging
+    log_level = args.log_level.strip()
+    try:
+        logging.basicConfig(level=eval('logging.%s' % log_level))
+    except AttributeError:
+        raise RuntimeError('No such log level: %s' % log_level)
+
+# process the range argument
+plotrange = [int(a) for a in args.range.split(',')]
+if plotrange[0] == -1:
+    plotrange = (0, plotrange[1])
+if (plotrange[1] != -1) and (plotrange[1] <= plotrange[0]):
+    raise RuntimeError('Plot range of %s is incorrect.' % str(plotrange))
+plotrange = (int(plotrange[0]), int(plotrange[1]))
+
+
+def exit_gracefully(_, __):
+    fpga.disconnect()
+    sys.exit(0)
+signal.signal(signal.SIGINT, exit_gracefully)
+
+# make the FPGA
+fpga = utils.feng_script_get_fpga(args)
+
+
+def plot_func(figure, sub_plots, idata, ictr, pctr):
+    data = get_data()
+    ictr += 1
+
+    topstop = plotrange[1] if plotrange[1] != -1 else len(data['p0'].data)
+
+    p0_data = data['p0'].data[plotrange[0]:topstop]
+    p1_data = data['p1'].data[plotrange[0]:topstop]
+
+    # print('\tMean:   %.10f' % numpy.mean(p0_data[1000:3000])
+    # print('\tStddev: %.10f' % numpy.std(p0_data[1000:3000])
+
+    if args.fft:
+        plot_data0 = numpy.abs(numpy.fft.fft(p0_data))
+        plot_data1 = numpy.abs(numpy.fft.fft(p1_data))
+    else:
+        plot_data0 = p0_data
+        plot_data1 = p1_data
+
+    if idata is None:
+        idata = [[0] * len(plot_data0), [0] * len(plot_data1)]
+    for ctr in range(0, len(plot_data0)):
+        idata[0][ctr] += plot_data0[ctr]
+        idata[1][ctr] += plot_data1[ctr]
+    if args.integrate != 0:
+        plot_data0 = idata[0]
+        plot_data1 = idata[1]
+
+    allzero0 = False
+    allzero1 = False
+    if not args.linear:
+        allzero0 = numpy.sum(plot_data0) == 0
+        allzero1 = numpy.sum(plot_data1) == 0
+
+    # actually draw the plots
+    for pol in [0, 1]:
+        sub_plots[pol].cla()
+        sub_plots[pol].set_title('p%i' % pol)
+        subplots[pol].grid(True)
+
+    if args.hist:
+        sub_plots[0].hist(plot_data0, bins=64, range=(-1.0, 1.0))
+        sub_plots[1].hist(plot_data1, bins=64, range=(-1.0, 1.0))
+    else:
+        if args.linear:
+            sub_plots[0].plot(plot_data0)
+            sub_plots[1].plot(plot_data1)
+        else:
+            if not allzero0:
+                sub_plots[0].semilogy(plot_data0)
+            else:
+                sub_plots[0].plot([0] * len(plot_data0))
+            if not allzero1:
+                sub_plots[1].semilogy(plot_data1)
+            else:
+                sub_plots[1].plot([0] * len(plot_data1))
+    figure.canvas.draw()
+    if ictr == args.integrate and args.integrate > 0:
+        idata = None
+        ictr = 0
+        pctr += 1
+    fig.canvas.manager.window.after(10, plot_func, figure,
+                                    sub_plots, idata, ictr, pctr)
+
+
+def get_data():
+    if 'snap_adc0_ss' not in fpga.snapshots.names():
+        fpga.registers.fft_shift.write(cdsnap_arm=0)
+        fpga.snapshots.snap_cd0_ss.arm()
+        fpga.snapshots.snap_cd1_ss.arm()
+        fpga.registers.fft_shift.write(cdsnap_arm=1)
+        time.sleep(0.1)
+        d0 = fpga.snapshots.snap_cd0_ss.read(arm=False)['data']
+        d1 = fpga.snapshots.snap_cd1_ss.read(arm=False)['data']
+        fpga.registers.fft_shift.write(cdsnap_arm=0)
+        rvp0 = []
+        rvp1 = []
+        for ctr in range(0, len(d0['d0'])):
+            for ctr2 in range(8):
+                rvp0.append(d0['d%i' % ctr2][ctr])
+            for ctr2 in range(4):
+                rvp1.append(d0['p1_d%i' % ctr2][ctr])
+            tmp = (d0['p1_d4_u8'][ctr] << 2) | d1['p1_d4_l2'][ctr]
+            rvp1.append(caspermem.bin2fp(tmp, 10, 9, True))
+            for ctr2 in range(5, 8):
+                rvp1.append(d1['p1_d%i' % ctr2][ctr])
+        return {'p0': fhost_fpga.AdcData(-1, rvp0),
+                'p1': fhost_fpga.AdcData(-1, rvp1)}
+    else:
+        return fpga.get_adc_snapshots()
+
+if 'snap_adc0_ss' not in fpga.snapshots.names():
+    print('Could not find ADC snapshots, attempting to use '
+          'CD snaps instead.')
+
+# set up the figure with a subplot to be plotted
+data = get_data()
+
+if args.noplot:
+    while True:
+        print(data)
+        time.sleep(1)
+        data = get_data()
+
+else:
+    fig = pyplot.figure()
+    subplots = []
+    num_plots = 2
+    for p in range(num_plots):
+        sub_plot = fig.add_subplot(num_plots, 1, p + 1)
+        subplots.append(sub_plot)
+    integrated_data = None
+    integration_counter = 0
+    plot_counter = 0
+    fig.canvas.manager.window.after(10, plot_func, fig,
+                                    subplots, integrated_data,
+                                    integration_counter, plot_counter)
+
+    pyplot.show()
+    print('Plot started.')
+
+    # wait here so that the plot can be viewed
+    print('Press Ctrl-C to exit...')
+    sys.stdout.flush()
+    while True:
+        time.sleep(1)
+
---
+++ Python_Scripts/Corr2/Scripts/corr2_fengine_quant_plot.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Plot the output of the quantiser snapshot.
+"""
+import time
+import argparse
+import sys
+import signal
+import numpy
+#from matplotlib import pyplot
+import math
+
+# force the use of the TkAgg backend
+import matplotlib
+matplotlib.use('TkAgg')
+
+from matplotlib import pyplot
+
+from corr2 import utils
+
+
+parser = argparse.ArgumentParser(
+    description='Display the output of the quantiser on an F-engine.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    '--fftshift', dest='fftshift', action='store', default=-1, type=int,
+    help='the FFT shift to set')
+parser.add_argument(
+    dest='host', type=str, action='store', default='',
+    help='the F-engine host to query, if an int, will be taken positionally '
+         'from the config file list of f-hosts')
+parser.add_argument(
+    '--config', dest='config', type=str, action='store', default='',
+    help='a corr2 config file, will use $CORR2INI if none given')
+parser.add_argument(
+    '--bitstream', dest='bitstream', type=str, action='store', default='',
+    help='a bitstream for the f-engine hosts (Skarabs need this given '
+         'if no config is found)')
+parser.add_argument(
+    '--range', dest='range', action='store', default='-1,-1',
+    help='range to plot, -1 means start/end')
+parser.add_argument(
+    '--hist', dest='hist', action='store_true', default=False,
+    help='plot a histogram of the incoming data')
+parser.add_argument(
+    '--vacc', dest='vacc', type=int, action='store', default=0,
+    help='plot an accumulation of the incoming data')
+parser.add_argument(
+    '--integrate', dest='integrate', type=int, action='store', default=0,
+    help='fft option - how many spectra should be integrated')
+parser.add_argument(
+    '--linear', dest='linear', action='store_true', default=False,
+    help='plot linear, not log')
+parser.add_argument(
+    '--print', dest='printvals', action='store_true', default=False,
+    help='print(the plotted values to the screen')
+parser.add_argument(
+    '--noplot', dest='noplot', action='store_true', default=False,
+    help='do not plot anything')
+parser.add_argument(
+    '--loglevel', dest='log_level', action='store', default='INFO',
+    help='log level to use, options INFO, DEBUG, ERROR')
+args = parser.parse_args()
+
+if args.log_level != '':
+    import logging
+    log_level = args.log_level.strip()
+    try:
+        logging.basicConfig(level=eval('logging.%s' % log_level))
+    except AttributeError:
+        raise RuntimeError('No such log level: %s' % log_level)
+
+if args.noplot and (not args.printvals):
+    raise RuntimeError('Must either plot or print(vals!')
+
+# read the config
+config = utils.parse_ini_file(args.config)
+
+EXPECTED_FREQS = int(config['fengine']['n_chans'])
+
+required_snaps = ['snap_quant0_ss', 'snap_quant1_ss']
+
+# process the range argument
+plotrange = [int(a) for a in args.range.split(',')]
+if plotrange[0] == -1:
+    plotrange = (0, EXPECTED_FREQS)
+if (plotrange[1] != -1) and (plotrange[1] <= plotrange[0]):
+    raise RuntimeError('Plot range of %s is incorrect.' % str(plotrange))
+plotrange = (int(plotrange[0]), int(plotrange[1]))
+
+
+def exit_gracefully(_, __):
+    fpga.disconnect()
+    sys.exit(0)
+signal.signal(signal.SIGINT, exit_gracefully)
+
+# make the FPGA
+fpga = utils.feng_script_get_fpga(args)
+
+# set the FFT shift
+if args.fftshift != -1:
+    fpga.registers.fft_shift.write_int(args.fftshift)
+current_fft_shift = fpga.registers.fft_shift.read()['data']['fft_shift']
+print('Current FFT shift for %s is %i.' % (fpga.host, current_fft_shift))
+
+# check the size of the snapshot blocks
+snap0 = fpga.snapshots.snap_quant0_ss
+nsamples = int(snap0.block_info['snap_nsamples'])
+dwidth = int(snap0.block_info['snap_data_width'])
+snapshot_bytes = (2**nsamples) * (dwidth/8)
+print('Snapshot is %i bytes long' % snapshot_bytes)
+snapshot_samples = (2**nsamples) * 4
+print('Snapshot is %i samples long' % snapshot_samples)
+# loops_necessary = EXPECTED_FREQS / snapshot_samples
+# print('Will need to read the snapshot %i times' % loops_necessary
+
+print('Range: %s' % str(plotrange))
+numtoplot = plotrange[1] - plotrange[0]
+print('Chans to plot: %s' % numtoplot)
+loopnec = int(math.ceil((numtoplot * 1.0) / snapshot_samples))
+loopstart = plotrange[0] / snapshot_samples
+loopstop = loopstart + loopnec
+plotrange_shifted = (plotrange[0] - (loopstart * snapshot_samples),
+                     plotrange[1] - (loopstart * snapshot_samples))
+
+# print('loopsnec:', loopnec
+# print('loopstart:', loopstart
+# print('loopstop:', loopstop
+# print('plotrange:', plotrange
+# print('plotrange_shifted:', plotrange_shifted
+# raise RuntimeError
+
+print('Will need to read the snapshot %i times' % (loopstop - loopstart))
+
+
+def get_data():
+    # read the data
+    snapdata = []
+    for loop_ctr in range(loopstart, loopstop):
+        temp_snapdata = {}
+        for snap in required_snaps:
+            offset = snapshot_bytes * loop_ctr
+            #print('reading %s at offset %i' % (snap, offset))
+            sys.stdout.flush()
+            temp_snapdata[snap] = fpga.snapshots[snap].read(offset=offset)
+        snapdata.append(temp_snapdata)
+
+    fpga_data = {'p0': [], 'p1': []}
+    pol_data = [[], []]
+    # reorder the data
+    for loop_ctr in range(loopstart, loopstop):
+        #print('calculating quant data for loop %i' % loop_ctr)
+        quant_data = [snapdata[loop_ctr][required_snaps[0]]['data'],
+                      snapdata[loop_ctr][required_snaps[1]]['data']]
+        #print('\tsnap was %i long' % len(quant_data[0]['real0']))
+        for polctr in range(0, 2):
+            for data_ctr in range(0, len(quant_data[0]['real0'])):
+                for valctr in range(0, 4):
+                    rval = quant_data[polctr]['real%i' % valctr][data_ctr]
+                    ival = quant_data[polctr]['imag%i' % valctr][data_ctr]
+                    cplx = numpy.complex(rval, ival)
+                    pol_data[polctr].append(cplx)
+        #print('\tpol_data is now %i long' % len(pol_data[0]))
+
+    if len(pol_data[0]) != len(pol_data[1]):
+        raise RuntimeError('Unequal length data for p0 and p1?!')
+    print('%s data is %d points long.' % (fpga.host, len(pol_data[0])))
+    assert len(pol_data[0]) >= numtoplot, 'Not enough snap data for the ' \
+                                          'required number of channels! %i - %i' % (len(pol_data[0]), numtoplot)
+    fpga_data['p0'] = pol_data[0][plotrange_shifted[0]:plotrange_shifted[1]]
+    fpga_data['p1'] = pol_data[1][plotrange_shifted[0]:plotrange_shifted[1]]
+    return fpga_data
+
+
+def plot_hist(figure, sub_plots, pctr):
+    data = get_data()
+    for ctr, integd in enumerate(data.values()):
+        plt = sub_plots[ctr]
+        plt[0].cla()
+        plt[0].set_title('%s:p%i real, %i' % (
+            fpga.host, ctr, pctr))
+        plt[0].grid(True)
+        plt[1].cla()
+        plt[1].set_title('%s:p%i imag, %i' % (
+            fpga.host, ctr, pctr))
+        plt[1].grid(True)
+        hdata = data['p%i' % ctr]
+        plt[0].hist(numpy.real(hdata), bins=32)
+        plt[1].hist(numpy.imag(hdata), bins=32)
+        pctr += 1
+    figure.canvas.draw()
+    pctr += 1
+    figure.canvas.manager.window.after(10, plot_hist, figure, sub_plots, pctr)
+
+
+def plot_spectrum(figure, sub_plots, idata, ictr, pctr):
+    data = get_data()
+    ictr += 1
+    p0_data = numpy.abs(data['p0'])
+    p1_data = numpy.abs(data['p1'])
+    if idata is None:
+        idata = {0: [0] * len(p0_data), 1: [0] * len(p1_data)}
+    for ctr in range(0, len(p0_data)):
+        idata[0][ctr] += p0_data[ctr]
+        idata[1][ctr] += p1_data[ctr]
+    # print('\tMean:   %.10f' % numpy.mean(p0_data[100:4000])
+    # print('\tStddev: %.10f' % numpy.std(p0_data[100:4000])
+    if args.printvals:
+        print(idata)
+    if args.noplot:
+        return
+    # actually draw the plots
+    for ctr, integd in enumerate(idata.values()):
+        plt = sub_plots[ctr]
+        plt.cla()
+        plt.set_title('%s:%i, %i, %i' % (fpga.host, ctr, ictr, pctr))
+        plt.grid(True)
+        if args.linear:
+            plt.plot(integd)
+        else:
+            if numpy.sum(integd) > 0:
+                plt.semilogy(integd)
+            else:
+                plt.set_title('%s:%i, %i, %i - NO NONZERO DATA' % (
+                    fpga.host, ctr, ictr, pctr))
+    figure.canvas.draw()
+    if ictr == args.integrate and args.integrate > 0:
+        idata = None
+        ictr = 0
+        pctr += 1
+    figure.canvas.manager.window.after(10, plot_spectrum, figure, sub_plots,
+                                       idata, ictr, pctr)
+
+def plot_spectrum_vacc(figure, sub_plots, idata, ictr, pctr):
+    print 'Vacc limit is %s', args.vacc
+    vacc_limit = args.vacc
+
+    data = get_data()
+    ictr += 1
+    print 'ictr is %s', ictr
+
+    p0_data = numpy.abs(numpy.power(data['p0'],2))
+    p1_data = numpy.abs(numpy.power(data['p1'],2))
+
+    if idata is None:
+        idata = {0: [0] * len(p0_data), 1: [0] * len(p1_data)}
+    for ctr in range(0, len(p0_data)):
+        idata[0][ctr] += p0_data[ctr]
+        idata[1][ctr] += p1_data[ctr]
+
+    if ictr < vacc_limit:
+        figure.canvas.manager.window.after(10, plot_spectrum_vacc, figure, sub_plots,
+                                           idata, ictr, pctr)
+    else:
+
+        if args.printvals:
+            print(idata)
+        if args.noplot:
+            return
+
+        # actually draw the plots
+        for ctr, integd in enumerate(idata.values()):
+            plt = sub_plots[ctr]
+            plt.cla()
+            plt.set_title('%s:%i, %i, %i' % (fpga.host, ctr, ictr, pctr))
+            plt.grid(True)
+            if args.linear:
+                plt.plot(integd)
+            else:
+                if numpy.sum(integd) > 0:
+                    plt.semilogy(integd)
+                else:
+                    plt.set_title('%s:%i, %i, %i - NO NONZERO DATA' % (
+                        fpga.host, ctr, ictr, pctr))
+        figure.canvas.draw()
+
+        from IPython import embed
+        embed()
+
+        idata = None
+        ictr = 0
+        pctr += 1
+
+        #if ictr == args.integrate and args.integrate > 0:
+        #    idata = None
+        #    ictr = 0
+        #    pctr += 1
+
+        figure.canvas.manager.window.after(10, plot_spectrum_vacc, figure, sub_plots,
+                                           idata, ictr, pctr)
+
+# print, no plot
+if args.printvals and args.noplot:
+    import sys
+    while True:
+        try:
+            d = get_data()
+            print('p0: %s' % d['p0'])
+            print('p1: %s' % d['p1'])
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+    sys.exit(0)
+
+# set up the figure with a subplot to be plotted
+fig = pyplot.figure()
+subplots = []
+plot_counter = 0
+if args.hist:
+        subplots.append((fig.add_subplot(2, 2, 1),
+                         fig.add_subplot(2, 2, 2)))
+        subplots.append((fig.add_subplot(2, 2, 3),
+                         fig.add_subplot(2, 2, 4)))
+        fig.canvas.manager.window.after(10, plot_hist, fig, subplots,
+                                        plot_counter)
+elif args.vacc:
+    subplots.append(fig.add_subplot(2, 1, 1))
+    subplots.append(fig.add_subplot(2, 1, 2))
+    integrated_data = None
+    integration_counter = 0
+    fig.canvas.manager.window.after(10, plot_spectrum_vacc, fig, subplots,
+                                    integrated_data, integration_counter,
+                                    plot_counter)
+else:
+    subplots.append(fig.add_subplot(2, 1, 1))
+    subplots.append(fig.add_subplot(2, 1, 2))
+    integrated_data = None
+    integration_counter = 0
+    fig.canvas.manager.window.after(10, plot_spectrum, fig, subplots,
+                                    integrated_data, integration_counter,
+                                    plot_counter)
+pyplot.show()
+print('Plot started.')
+
+# wait here so that the plot can be viewed
+print('Press Ctrl-C to exit...')
+sys.stdout.flush()
+while True:
+    time.sleep(1)
+
+# end
