f272f08378d86d31dcefbe0064ac6ae93d713046
andrewvanderbyl <avanderbyl@ska.ac.za>
2023-03-16 13:12:45 +0200

    Code clean up.
    Started adding 2nd stage for PFB channeliser.

--- Matlab/Polyphase/GreenPFB/demo_green_pfb.m
+++ Matlab/Polyphase/GreenPFB/demo_green_pfb.m
@@ -20,13 +20,10 @@ clear
 %% Phase 1: Generate signals
 % --- Step 1: Set parameters ---:
 fft_length = 32768;
-
-% Mixer: Upconverter parameters
 amplitude = 1;
 fs_freq = 2.8e9;
 bw = 0e6;
-bin_width = fs_freq/fft_length;
-% fs_rotator = cw_freq*2;
+
 num_cycles = 25000;
 
 % Baseband signal parameters
@@ -39,10 +36,6 @@ load stage1_coeffs.mat
 
 % --- Step 2: Generate signals ---:
 % Generate baseband signal (complex as to emulate pol0 and pol 1)
-%upsample_ratio = cw_freq/(bb_signal_freq_bw_high*2);
-%baseband_cycles = rotator_cycles/upsample_ratio;
-%fs_baseband = bb_signal_freq_bw_high*2*upsample_ratio*2; %first x2 as we need Nyquist rate; 2nd x2 as we Nyquist rate for upconversion rotator
-
 [bb_signal_bw_low_real, bb_signal_bw_low_cmplx, ~, ~] = Sig_Gen(bb_signal_freq_bw_low, num_cycles, fs_freq, amplitude, 10);
 [bb_signal_if_real, bb_signal_if_cmplx, ~, ~] = Sig_Gen(bb_signal_freq_if, num_cycles, fs_freq, amplitude, 10);
 [bb_signal_bw_high_real, bb_signal_bw_high_cmplx, ~, ~] = Sig_Gen(bb_signal_freq_bw_high, num_cycles, fs_freq, amplitude, 10);
@@ -50,21 +43,10 @@ load stage1_coeffs.mat
 common_length = length(bb_signal_bw_high_cmplx);
 baseband_signal_cmplx = bb_signal_bw_low_cmplx(1:common_length) + bb_signal_if_cmplx(1:common_length) + bb_signal_bw_high_cmplx;
 
-% Generate complex rotator
-%[rotator_real,rotator_cmplx,~,~] = Sig_Gen(cw_freq,rotator_cycles, fs_rotator, amplitude, 10);
-
-% Upconvert complex signal
-%upsampled_signal = baseband_signal_cmplx(1:fft_length).*rotator_cmplx(1:fft_length);
-
-% create equivalent upsampled IF signal. This should be the baseband IF
-% with the baseband IF centered around fs (in frequency domain).
-% full_signal = upsampled_signal + baseband_signal_cmplx(1:fft_length);
-%full_signal = upsampled_signal;
-
 % Compute FFT on complex heterodyned signal and on each polarisation.
-fft_pol_cmplx = fft(baseband_signal_cmplx, fft_length);
-fft_pol0 = fft(real(baseband_signal_cmplx), fft_length);
-fft_pol1 = fft(imag(baseband_signal_cmplx), fft_length);
+% fft_pol_cmplx = fft(baseband_signal_cmplx, fft_length);
+% fft_pol0 = fft(real(baseband_signal_cmplx), fft_length);
+% fft_pol1 = fft(imag(baseband_signal_cmplx), fft_length);
 
 % Plot spectra
 % figure(1);
@@ -75,9 +57,6 @@ fft_pol1 = fft(imag(baseband_signal_cmplx), fft_length);
 % subplot(3,1,3)
 % stem(abs(fft_pol0.^2))
 % stem(abs(fft_pol1.^2))
-
-%resample_factor = 8;
-%[resampled_bb_signal] = resample_series(baseband_signal_cmplx, resample_factor);
 
 %% Phase 2: Decompose signal and filter coefficients into M-paths and process each path
 
@@ -96,15 +75,31 @@ filter_out = zeros(size_data_matrix(1,1), size_data_matrix(1,2)-1 + size_coeff_m
 
 % --- Step 4: Process each path ---:
 for row = 1:size_coeff_matrix(1,1)
-    filter_out(row,:) = conv(M_path_data(row,1:end), M_path_coeffs(row,1:end));
+    tmp_real = conv(real(M_path_data(row,1:end)), M_path_coeffs(row,1:end));
+    tmp_imag = conv(imag(M_path_data(row,1:end)), M_path_coeffs(row,1:end));
+    filter_out(row,:) = tmp_real+1i*tmp_imag;
+   
 end
 
 % --- Step 5: Add paths together to get rate reduced output ---:
 pfb_stage1_output = sum(filter_out);
-stem(abs(fft(pfb_stage1_output,fft_length)));
+%stem(abs(fft(pfb_stage1_output,fft_length)));
 
 
-%% Phase 3:
+%% Phase 3: Decompose 1st stage to form 2nd stage PFB for channeliser
+% --- Step 6: Split into N-paths ---:
+N = 4096;
+
+rounded_split_length = floor(length(pfb_stage1_output)/N);
+
+% Coeffs
+[N_path_coeffs] = m_path_split(pfb_stage1_output(1:rounded_split_length), N);
+
+% Signal
+[N_path_data] = m_path_split(pfb_stage1_output(1:rounded_split_length), N);
 
 
-a = 1;
+% --- Step 7: Process each path ---:
+
+
+
